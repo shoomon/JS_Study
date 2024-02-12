ac1fd00c4ba9010cba816381202b9ea209b03edb
shoomon <kate010117@naver.com>
2024-02-12 23:08:09 +0900

    24.02.12 Leared Typescript

---
+++ Typescript - Nomard Coder/TypeChain/src/myPackage.js
@@ -0,0 +1,20 @@
+// @ts-check
+//JSDoc
+/**
+ * intializes the project
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
+export function init(config) {
+    return true;
+}
+/**
+ * Exit the program
+ * @param {number} code 
+ * @returns number
+ */
+export function exit(code) {
+    return code + 1;
+}
